edd2d8d43be6fcf45d84c4dfbe64fc2c0259d6b2
Andrew Liu <liu.andrew1988@gmail.com>
2021-03-01 13:15:19 -0500

    create controller for series

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const { Router } = require('express')
+const showControllers = require('../controllers/index')
+
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/createShow', showControllers.createShow)
+router.get('/getAllShows', showControllers.getShows)
 
 module.exports = router
